49d60e9e4773b28cb3f15eef7fdefff08b05a0f7
amir58 <devamir11@gmail.com>
2022-03-18 14:52:41 +0200

    Update design

--- app/src/main/java/com/amirmohammed/fit/ui/WelcomeActivity.java
+++ app/src/main/java/com/amirmohammed/fit/ui/WelcomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.amirmohammed.fit.R;
 
@@ -15,6 +16,10 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
     }
 
     public void startNow(View view) {
