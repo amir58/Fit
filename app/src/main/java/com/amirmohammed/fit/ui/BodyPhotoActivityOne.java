50b95320e3f082429b917ed8f40312fc8c0c92c4
Abdullah Amin <bido6573@gmail.com>
2022-03-19 18:03:59 +0200

    finished BodyPhotoActivityOne

---
+++ app/src/main/java/com/amirmohammed/fit/ui/BodyPhotoActivityOne.java
@@ -0,0 +1,20 @@
+package com.amirmohammed.fit.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.amirmohammed.fit.R;
+
+public class BodyPhotoActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_body_photo_one);
+    }
+
+    public void back(View view) {
+    }
+}
