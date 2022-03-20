eef577550b9c260971914f142e398da241dba4fe
Abdullah Amin <bido6573@gmail.com>
2022-03-20 19:15:05 +0200

    finished FrontBodyPhotoActivityOne + BackBodyPhotoActivityOne + InBodyActivity + PhotoOrInBodyLoadingActivity + HomeTrainActivityOne + HomeTrainActivityTwo

---
+++ app/src/main/java/com/amirmohammed/fit/ui/HomeTrainActivityTwo.java
@@ -0,0 +1,14 @@
+package com.amirmohammed.fit.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeTrainActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_train_two);
+    }
+}
